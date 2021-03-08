4b1e0ff54a01901eded351a2edefef0cbf844011
Student User <student@workstation.lab.example.com>
2021-03-08 08:32:07 -0500

    hello

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,11 +1,11 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
+print "Goodbye, World! php version is " . PHP_VERSION . "\n";
 ?>
